c6f9833e85a43a2ce9b39428407c0487ed5c881f
kimkyungm13 <kyung_m13@naver.com>
2024-02-20 17:34:36 +0900

    12

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,17 +2,35 @@ gsap.from('.section1 h2 span', {
     y: 100, duration: 1, stagger: {
         from: 'start',
         each: 0.3
-    }, opacity: 0
+    },
 })
 
-ScrollTrigger.create({
-    trigger: 'section',
-    toggleClass: {
-        targets: 'header',
-        className: 'none',
 
+const sc01 = gsap.timeline();
+sc01.to('.section1 h2 span', { y: innerHeight * -1, autoAlpha: 0 }).to('.section1 img', {
+    autoAlpha: 1,
+});
+ScrollTrigger.create({
+    animation: sc01,
+    trigger: '.section1',
+    pin: true,
+    scrub: true,
+    end: "+=1200vh",
+    toggleActions: "play none none none",
+});
+
+
+
+gsap.to('.section2 .sc02-tit', {
+    scrollTrigger: {
+        trigger: '.section2',
+        start: '20% 10%',
+        end: "+=600vh",
+        className: 'ispaly',
+        markers: true,
+        pin: true,
+        scrub: true,
     },
-    start: '20% 0%',
-    end: '100% 0%',
-    markers: true
+    autoAlpha: 0,
+    duration: 2,
 })
