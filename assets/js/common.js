13f172965161b46dcd5bbd9aa3d199387b388c0a
kimkyungm13 <kyung_m13@naver.com>
2024-02-22 16:23:27 +0900

    0222

--- assets/js/common.js
+++ assets/js/common.js
@@ -27,18 +27,72 @@ gsap.to('.section2 .sc02-tit', {
         start: '20% 10%',
         end: "+=600vh",
         className: 'ispaly',
-        markers: true,
+
         pin: true,
         scrub: true,
     },
     autoAlpha: 0,
     duration: 2,
-})
-const panel = document.querySelector(".section2");
+});
 
 ScrollTrigger.create({
-    trigger: panel,
+    trigger: ".section2",
     start: 'top top',
     pin: true,
     pinSpacing: false
+});
+
+const sc03 = gsap.timeline();
+sc03.from(['.section3 .hd-tit', '.section3 .hd-txt p'], {
+    y: 100, stagger: {
+        from: 'start',
+        each: 0.3
+    }, duration: 1,
+    autoAlpha: 0
 })
+ScrollTrigger.create({
+    animation: sc03,
+    trigger: '.section3',
+    start: 'top 80%',
+
+})
+gsap.utils.toArray(".link-group a").forEach((item, index) => {
+    gsap.from(item, {
+        yPercent: 100,
+        autoAlpha: 0,
+        duration: 0.5,
+        ease: "none",
+        stagger: {
+            each: 0.4 * index, // 각 요소에 대한 애니메이션 시작 시간 간격
+            from: "start" // 첫 번째 요소부터 시작
+        },
+
+        scrollTrigger: {
+            trigger: item,
+            start: "top bottom",
+            markers: true,
+        }
+    })
+});
+// sc03.from('.link-group .link01', {
+//     y: 100, autoAlpha: 0, stagger: 0.4, scrollTrigger: {
+//         trigger: '.link-group .link01',
+//         start: 'top 80%',
+//         scrub: true,
+//         markers: true,
+//     }
+// }).from('.link-group .link02', {
+//     y: 100, autoAlpha: 0, stagger: 0.4, scrollTrigger: {
+//         trigger: '.link-group .link02',
+//         start: 'top 80%',
+//         scrub: true,
+//         markers: true,
+//     }
+// }).from('.link-group .link03', {
+//     y: 100, autoAlpha: 0, stagger: 0.4, scrollTrigger: {
+//         trigger: '.link-group .link03',
+//         start: 'top 80%',
+//         scrub: true,
+//         markers: true,
+//     }
+// });
